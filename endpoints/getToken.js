a9cec5a1d75b989f1e4fd4b5f5482f3f3fcbc3e8
Ollie <3martynov@gmail.com>
2021-07-20 16:44:45 -0400

    more changes

--- endpoints/getToken.js
+++ endpoints/getToken.js
@@ -2,22 +2,33 @@ const generator = require("../metaGenerator");
 const config = require("../config");
 const imageGenerator = require("../imageGenerator");
 const imageStorage = require('../imageStorage');
-const { getMeta } = require('../metaStorage');
+const { getMeta, saveMeta } = require('../metaStorage');
 
 module.exports.get = async (event, context, callback) => {
   //todo: check if token id has been minted before creating new asset
   const id = event["pathParameters"]["id"];
   console.log({ id });
-  const resp = await getMeta(id);
-  const { meta, assets } = generator(config, id);
-  const buffer = await imageGenerator(assets);
-  console.log("got generated image buffer")
-  await imageStorage(buffer, id);
-  console.log("saved to storage")
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(meta),
-  };
-
-  callback(null, response);
+  const existingMeta = await getMeta(id);
+  if(existingMeta){
+    console.log("... got existing Meta")
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(existingMeta),
+    };
+    callback(null, response);
+  }else{
+    console.log("... creating new Meta")
+    const { meta, assets } = generator(config, id);
+    const buffer = await imageGenerator(assets);
+    console.log("got generated image buffer")
+    await imageStorage(buffer, id);
+    console.log("saved image to storage")
+    const dbResp = await saveMeta(id, meta);
+    console.log(dbResp);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(meta),
+    };
+    callback(null, response);
+  }
 };
--- metaStorage.js
+++ metaStorage.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = require('./dynamodb');
 
 module.exports.getMeta = async function getMeta(id){
     const params = {
@@ -10,5 +10,24 @@ module.exports.getMeta = async function getMeta(id){
       };
     
       const resp = await dynamoDb.get(params).promise();
-      console.log(resp);
+      console.log({resp});
+      return resp.Item?resp.Item:null;
 }
+
+module.exports.saveMeta = (id, meta) => {
+    console.log("... saving to db")
+    const timestamp = new Date().getTime();
+  
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: id,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+        ...meta
+      },
+    };
+  
+    // write the todo to the database
+    return dynamoDb.put(params).promise();
+  };
